9efc35cfd7792541f799f25345a37ec2a19af19c
vova.dmytriukhin <volodymyr.dmytriukhin@dev.pro>
2022-11-11 16:53:26 +0200

    fixed readme file

--- webdriver_test2/readme.md
+++ webdriver_test2/readme.md
@@ -4,6 +4,6 @@ s
 ## Run the tests
 
 1. run the selenium image with one of the following command:
-    1.1. `cd grid`, `docker run -d -p 4444:4444 -p 5901:5900 -v /Users/vova.dmytriukhin/Projects/webdriver_test2/webdriver_test2/grid/config.toml:/opt/bin/config.toml selenium/standalone-chrome `
+    1.1. `cd grid`, `docker run -d -p 4444:4444 -p 5901:5900 -v /Users/.../Projects/webdriver_test2/webdriver_test2/grid/config.toml:/opt/bin/config.toml selenium/standalone-chrome `
     1.2. `cd selenoid`, `docker-compose up`
 2. run tests with `dotnet test`
---
+++ webdriver_test2/tests/SecondTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using webdriver_test2.PO;
+
+namespace webdriver_test2
+{
+    [AllureNUnit]
+    public class SecondTest : TestBaseSetup
+    {
+        HomePage homePage;
+        LoginPage loginPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            homePage = new HomePage(driver);
+            loginPage = new LoginPage(driver);
+        }
+
+
+        [Test]
+        [TestCase(TestName = "Second Test", Description = "This test uses a simple input value")]
+        [AllureTag("Regression")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureIssue("ISSUE-1")]
+        [AllureTms("TMS-12")]
+        [AllureOwner("User")]
+        [AllureSuite("PassedSuite")]
+        [AllureSubSuite("NoAssert")]
+        public void Test2()
+        {
+            homePage.open();
+            homePage.loginBtn.click();
+            loginPage.userName.fill(Environment.GetEnvironmentVariable("user_name"));
+            loginPage.password.fill(Environment.GetEnvironmentVariable("user_password"));
+            loginPage.loginBtn.click();
+            Assert.Pass();
+        }
+    }
+}
